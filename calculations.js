30a21d5e20239bf2b8f5114c2af6f807c82fc1d7
Tion Blackmon <tionblackmon@icloud.com>
2024-07-09 22:02:26 -0500

    Added some event listeners that changes the display to 0 when the user clicks the clear button (c). Also replicated the same behavior for the 7 button.

--- calculations.js
+++ calculations.js
@@ -1,3 +1,12 @@
 document.getElementById('clear-btn').addEventListener('click', () => {
   document.getElementById('display-number').innerHTML = '0';
 });
+
+const sevenButton = document.getElementById('seven-btn');
+sevenButton.addEventListener('click', () => {
+  const number = sevenButton.innerHTML;
+  document.getElementById('display-number').innerHTML = number;
+});
+
+const eightButton = document.getElementById('eight-btn');
+console.log(eightButton.innerHTML);
